d64e94a2dda8e3e0e693f7b9dedf877d928372a8
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2024-04-24 13:48:36 -0400

    added linking to home

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,11 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Styles/Home.css";
+import inventoryData from "./Data/invertoryData.json";
+import Search from "./Search";
+import { Link } from "react-router-dom";
 
-const Display = () => {
+// Define the InventoryItem interface here
+interface InventoryItem {
+  image?: string;
+  title: string;
+  price: string;
+  quantity: number;
+}
+
+const Home = () => {
+  const [inventory, setInventory] = useState<InventoryItem[]>([]);
+
+  const handleAddToCart = (item: InventoryItem) => {
+    console.log("Add to Cart", item);
+  };
+
+  useEffect(() => {
+    setInventory(inventoryData);
+  }, []);
+
   return (
     <React.Fragment>
-        <h1>Display</h1>
+      <Search />
+      <h2>Prime Picks</h2>
+      <strong>Display</strong>
+      <div className='outer'>
+        <div className='inventory-details'>
+          {inventory.map((item, index) => (
+            <div key={index} className='inventory-card outer'>
+              <img
+                src={item.image || "/static/images/coverNotFound.png"}
+                alt='Inventory Item Cover'
+                style={{ maxWidth: "100px", maxHeight: "150px" }}
+              />
+              <div className='inventory-description'>
+                <p>
+                  <strong>Title:</strong> {item.title}
+                  <br />
+                  <strong>Price:</strong> {item.price}
+                  <br />
+                  <strong>In Stock:</strong> {item.quantity}
+                  <br />
+                  <button onClick={() => handleAddToCart(item)}>
+                    Add to Cart
+                  </button>
+                </p>
+              </div>
+            </div>
+          ))}
+          <Link to='/display'>View All</Link>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
 
-export default Display;
+export default Home;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Styles/Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <a href="home">
-        <img src={require('./Images/home.png')} alt="Home" />
+    <div className='navbar'>
+      <a href='home'>
+        <Link to='/'>
+          <img src={require("./Images/home.png")} alt='Home' />
+        </Link>
       </a>
-      <a href="login">
-        <img alt="Login" />
+      <a href='login'>
+        <img alt='Login' />
       </a>
     </div>
   );
