779b1b5e8b26fc2a0e9efb63862e387910a510ca
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2024-04-25 14:43:24 -0400

    added description

--- src/components/ViewDetials.tsx
+++ src/components/ViewDetials.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 import inventoryData from "./Data/invertoryData.json";
+import "./Styles/Home.css";
 
 interface InventoryItem {
   id: number;
@@ -8,18 +9,20 @@ interface InventoryItem {
   title: string;
   price: string;
   quantity: number;
+  description: string;
 }
 
 const ViewDetails = () => {
   // Retrieve the `id` parameter from the URL and provide a default value if undefined
   const { id } = useParams<{ id?: string }>();
-  
+
   // Handle the case where `id` could be undefined by defaulting to "0"
   const itemId = parseInt(id ?? "0", 10);
 
   // Find the item or return null if `itemId` is not a valid number
-  const item = isNaN(itemId) ? null : inventoryData.find((item: InventoryItem) => item.id === itemId);
-
+  const item = isNaN(itemId)
+    ? null
+    : inventoryData.find((item: InventoryItem) => item.id === itemId);
 
   if (!item) {
     return <div>Item not found</div>;
@@ -30,12 +33,14 @@ const ViewDetails = () => {
       <img
         src={item.image || "/static/images/coverNotFound.png"}
         alt='Detail Item Cover'
-        style={{ width: '200px', height: '300px' }}
+        style={{ width: "200px", height: "300px" }}
       />
-      <h2>{item.title}</h2>
-      <p><strong>Price:</strong> {item.price}</p>
-      <p><strong>Quantity:</strong> {item.quantity}</p>
-      <p><strong>Description:</strong> Description</p>
+      <p>
+        <strong>{item.title}</strong>
+        <strong>Price:</strong> {item.price}
+        <strong>Quantity:</strong> {item.quantity}
+        <strong>Description:</strong> {item.description}
+      </p>
     </div>
   );
 };
